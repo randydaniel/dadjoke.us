b357fc563c6d1bc0c39cb65bdb09114493cb8148
@randydaniel <romeomikedelta@gmail.com>
2020-01-25 15:37:24 -0500

    Update footer.js
    Adding Contributors.

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,7 +4,7 @@ import './Footer.scss';
 function Footer() {
   return (
     <footer>
-      <small>Made with <span role="img" aria-labelledby="Pizza">🍕</span> by <a href="https://randy-daniel.com">Randy Daniel</a>.</small>
+      <small>Made with <span role="img" aria-labelledby="Pizza">🍕</span> by <a href="https://randy-daniel.com">Randy Daniel</a>.<br />Code contribution(s): <a href="https://github.com/kanghamin" target="_blank" rel="noopener">Hamin Kang</a>.</small>
     </footer>
   );
 }
